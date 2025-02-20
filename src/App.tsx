64fe5254f3b97117e8d8b33e3c2a0f91e226a981
Triyo Krismantoro <rusdiantoro117@gmail.com>
2025-02-20 09:22:16 +0700

    dev: add CRUD todo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,77 @@
+import { useState } from "react";
+import { todoData } from "./todoData";
+import { TodoItem } from "./todoData";
+
 function App() {
+  const [todos, setTodos] = useState<TodoItem[]>(todoData);
+  const [inputTodo, setInputTodo] = useState<string>("");
+
+  const handleAddTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodos((prev) => [...prev, { id: todos.length + 1, title: inputTodo }]);
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete "${
+          todos.find((todo) => todo.id === id)?.title
+        }"?`
+      )
+    ) {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
   return (
     <>
       <title>To Do List</title>
-      <div className='container min-h-screen font-sans text-gray-800 space-y-8'>
-        <div className='mt-8 flex justify-between'>
-          <h1 className='text-xl font-bold'>To Do List</h1>
-          <div className='flex gap-3'>
-            <button className='py-1 px-3 transition-colors duration-200 bg-sky-600 text-white text-sm font-semibold rounded-md hover:bg-sky-700'>
+      <div className="container min-h-screen font-sans text-gray-800 space-y-8">
+        <div className="mt-8 flex justify-between">
+          <h1 className="text-xl font-bold">To Do List</h1>
+          <div className="flex gap-3">
+            <button className="py-1 px-3 transition-colors duration-200 bg-sky-600 text-white text-sm font-semibold rounded-md hover:bg-sky-700">
               Login
             </button>
-            <button className='py-1 px-3 transition-colors duration-200 bg-sky-600 text-white text-sm font-semibold rounded-md hover:bg-sky-700'>
+            <button className="py-1 px-3 transition-colors duration-200 bg-sky-600 text-white text-sm font-semibold rounded-md hover:bg-sky-700">
               Sign Up
             </button>
           </div>
         </div>
-        <form className='flex gap-3'>
+        <form onSubmit={handleAddTodo} className="flex gap-3">
           <input
-            name='title'
-            placeholder='Title'
-            className='w-full py-3 px-5 border border-gray-500 rounded-md placeholder:text-gray-400'
+            name="title"
+            placeholder="Title"
+            required
+            onChange={(e) => setInputTodo(e.target.value)}
+            className="w-full py-3 px-5 border border-gray-500 rounded-md placeholder:text-gray-400"
           />
           <button
-            type='submit'
-            className='py-3 px-5 transition-colors duration-200 bg-sky-600 text-white font-semibold rounded-md hover:bg-sky-700'
+            type="submit"
+            className="py-3 px-5 transition-colors duration-200 bg-sky-600 text-white font-semibold rounded-md hover:bg-sky-700"
           >
             Add
           </button>
         </form>
-        <div className='space-y-3'>
-          <div className='flex justify-between items-center gap-3 py-4 px-5 bg-sky-100 rounded-md'>
-            <h3>To Do Title 1</h3>
-            <div>
-              <button className='size-4 transition-colors duration-200 fill-red-500 hover:fill-red-600'>
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-                  <path d='M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z' />
-                </svg>
-              </button>
+        <div className="space-y-3">
+          {todos.map((todo) => (
+            <div
+              key={todo.id}
+              className="flex justify-between items-center gap-3 py-4 px-5 bg-sky-100 rounded-md"
+            >
+              <h3>{todo.title}</h3>
+              <div>
+                <button
+                  onClick={() => handleDeleteTodo(todo.id)}
+                  className="size-4 transition-colors duration-200 fill-red-500 hover:fill-red-600"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z" />
+                  </svg>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className='flex justify-between items-center gap-3 py-4 px-5 bg-sky-100 rounded-md'>
-            <h3>To Do Title 2 with very very very very long description</h3>
-            <div>
-              <button className='size-4 transition-colors duration-200 fill-red-500 hover:fill-red-600'>
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-                  <path d='M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z' />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className='flex justify-between items-center gap-3 py-4 px-5 bg-sky-100 rounded-md'>
-            <h3>To Do Title 3</h3>
-            <div>
-              <button className='size-4 transition-colors duration-200 fill-red-500 hover:fill-red-600'>
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-                  <path d='M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z' />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className='flex justify-between items-center gap-3 py-4 px-5 bg-sky-100 rounded-md'>
-            <h3>To Do Title 4</h3>
-            <div>
-              <button className='size-4 transition-colors duration-200 fill-red-500 hover:fill-red-600'>
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-                  <path d='M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z' />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className='flex justify-between items-center gap-3 py-4 px-5 bg-sky-100 rounded-md'>
-            <h3>To Do Title 5</h3>
-            <div>
-              <button className='size-4 transition-colors duration-200 fill-red-500 hover:fill-red-600'>
-                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-                  <path d='M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z' />
-                </svg>
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
---
+++ src/todoData/index.ts
@@ -0,0 +1,10 @@
+export type TodoItem = {
+  id: number;
+  title: string;
+};
+
+export const todoData: TodoItem[] = [
+  { id: 1, title: 'To Do Title 1'},
+  { id: 2, title: 'To Do Title 2 with very very very very long description'},
+  { id: 3, title: 'To Do Title 3'},
+];
